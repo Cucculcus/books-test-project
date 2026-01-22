33df22aaec771bfb3a1522e549fdc14c927d7852
Cucculcus <dnazarov2005@yandex.ru>
2026-01-22 23:49:53 +0300

    fix configs to deploy

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import TheBoard from '@/views/TheBoard.vue'
 import BookComponent from '@/components/BookComponent.vue'
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     { path: '/', component: TheBoard },
     { path: '/book/:id', name: 'BookComponent', component: BookComponent },
